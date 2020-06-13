26554b873534175ef035afe63739d27b675ed6b7
Edgar Vasquez <pruebapagina25@gmail.com>
2020-06-13 01:24:24 -0700

    Version 2
    Se elimina vista empleado

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -44,10 +44,9 @@
                             <a class="nav-link" href="{{ route('serie.index') }}">{{ __('Series') }}</a>
 
                         </li>
-                        <li class="nav-item">
+                        <!-- <li class="nav-item">
                             <a class="nav-link" href="{{ route('empleados.index') }}">{{ __('Empleados') }}</a>
-
-                        </li>
+                        </li> -->
                         <li class="nav-item">
                             <a class="nav-link" href="{{ route('libros.index') }}">{{ __('Libros') }}</a>
 
